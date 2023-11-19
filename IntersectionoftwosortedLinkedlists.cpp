8b7da3ad8d021a3bdd1d38bac1c590ef7932e741
Digvijay Chavan <chavandigvijay9081@gmail.com>
2023-11-19 20:34:17 +0530

    Easy

---
+++ IntersectionoftwosortedLinkedlists.cpp
@@ -0,0 +1,72 @@
+// Given two linked lists sorted in increasing order, create a new linked list representing the intersection of the two linked lists. The new linked list should be made with without changing the original lists.
+
+// Note: The elements of the linked list are not necessarily distinct.
+
+// Example 1:
+
+// Input:
+// LinkedList1 = 1->2->3->4->6
+// LinkedList2 = 2->4->6->8
+// Output: 2 4 6
+// Explanation: For the given two
+// linked list, 2, 4 and 6 are the elements
+// in the intersection.
+// Example 2:
+
+// Input:
+// LinkedList1 = 10->20->40->50
+// LinkedList2 = 15->40
+// Output: 40
+// Your Task:
+// You don't have to take any input of print anything. Your task is to complete the function findIntersection(), which will take head of both of the linked lists as input and should find the intersection of two linked list and add all the elements in intersection to the third linked list and return the head of the third linked list.
+
+// Expected Time Complexity : O(n+m)
+// Expected Auxilliary Space : O(n+m)
+// Note: n, m are the size of the respective linked lists.
+
+// Constraints:
+// 1 <= size of linked lists <= 5000
+// 1 <= Data in linked list nodes <= 104
+
+/*
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+    public:
+    Node* findIntersection(Node* h, Node* j){
+        Node *p=NULL,*t=NULL;
+        while(h!=NULL && j!=NULL){
+            if(h->data == j->data){
+                if(t==NULL){
+                    t=j;
+                    p=j;
+                }
+	            else{
+	                p->next=j;
+	                p=j;
+	            }
+	            j=j->next;
+	            h=h->next;
+            }
+            else if(h->data < j->data){
+                h=h->next;
+            }
+            else {
+                j=j->next;
+            }
+        }
+        if(p!=NULL)p->next=NULL;
+        return t;
+    }
+};
