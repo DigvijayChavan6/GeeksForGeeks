7534a4e76ce074b3925a63905e8f36ee357dca75
Digvijay Chavan <chavandigvijay9081@gmail.com>
2023-12-09 18:42:57 +0530

    Easy

---
+++ Given a linked list of 0s, 1s and 2s, sort it.cpp
@@ -0,0 +1,74 @@
+// Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
+
+// Example 1:
+
+// Input:
+// N = 8
+// value[] = {1,2,2,1,2,0,2,2}
+// Output: 0 1 1 2 2 2 2 2
+// Explanation: All the 0s are segregated
+// to the left end of the linked list,
+// 2s to the right end of the list, and
+// 1s in between.
+// Example 2:
+
+// Input:
+// N = 4
+// value[] = {2,2,0,1}
+// Output: 0 1 2 2
+// Explanation: After arranging all the
+// 0s,1s and 2s in the given format,
+// the output will be 0 1 2 2.
+// Your Task:
+// The task is to complete the function segregate() which segregates the nodes in the linked list as asked in the problem statement and returns the head of the modified linked list. The printing is done automatically by the driver code.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(N).
+
+// Constraints:
+// 1 <= N <= 106
+
+/*
+ 
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        Node *p=head,*e=head;
+        int z=0,o=0,t=0;
+        while(e!=NULL){
+            if(e->data==0)z++;
+            else if(e->data==1)o++;
+            else t++;
+            e=e->next;
+        }
+        while(p!=NULL){
+            if(z>0){
+                p->data=0;
+                z--;
+            }
+            else if(o>0){
+                p->data=1;
+                o--;
+            }
+            else{
+                p->data=2;
+                t--;
+            }
+            p=p->next;
+        }
+        return head;
+    }
+};
