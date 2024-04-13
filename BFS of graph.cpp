1381c95d6d9780c3856e4850c693e42080b079ef
Digvijay Chavan <chavandigvijay9081@gmail.com>
2024-04-13 21:18:26 +0530

    Easy

---
+++ BFS of graph.cpp
@@ -0,0 +1,71 @@
+// Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0.
+// Note: One can move from node u to node v only if there's an edge from u to v. Find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the input graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
+
+
+// Example 1:
+
+// Input:
+// V = 5, E = 4
+// adj = {{1,2,3},{},{4},{},{}}
+
+
+// Output: 
+// 0 1 2 3 4
+// Explanation: 
+// 0 is connected to 1 , 2 , 3.
+// 2 is connected to 4.
+// so starting from 0, it will go to 1 then 2
+// then 3. After this 2 to 4, thus bfs will be
+// 0 1 2 3 4.
+
+// Example 2:
+
+// Input:
+// V = 3, E = 2
+// adj = {{1,2},{},{}}
+
+// Output: 
+// 0 1 2
+// Explanation:
+// 0 is connected to 1 , 2.
+// so starting from 0, it will go to 1 then 2,
+// thus bfs will be 0 1 2. 
+
+
+// Your task:
+// You dont need to read input or print anything. Your task is to complete the function bfsOfGraph() which takes the integer V denoting the number of vertices and adjacency list as input parameters and returns  a list containing the BFS traversal of the graph starting from the 0th vertex from left to right.
+
+
+// Expected Time Complexity: O(V + E)
+// Expected Auxiliary Space: O(V)
+
+
+// Constraints:
+// 1 ≤ V, E ≤ 104
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> vec;
+        getBfs(V, adj, vec);
+        return vec;
+    }
+    void getBfs(int V, vector<int> adj[], vector<int> &vec){
+        queue<int> q;
+        vector<bool> visited(V,false);
+        q.push(0);
+        while(!q.empty()){
+            int node=q.front();
+            if(!visited[node]){
+                visited[node]=true;
+                vec.push_back(node);
+                int s=adj[node].size();
+                for(int i=0;i<s;i++){
+                    if(!visited[adj[node][i]])q.push(adj[node][i]);
+                }
+            }
+            q.pop();
+        }
+    }
+};
