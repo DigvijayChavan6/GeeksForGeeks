adf0e716cf2f59c7829476bc7301436f793379d3
Digvijay Chavan <chavandigvijay9081@gmail.com>
2024-06-29 17:13:13 +0530

    Easy

---
+++ Identical Linked Lists.cpp
@@ -0,0 +1,49 @@
+// Given the two singly Linked Lists respectively. The task is to check whether two linked lists are identical or not. 
+// Two Linked Lists are identical when they have the same data and with the same arrangement too. If both Linked Lists are identical then return true otherwise return false. 
+
+// Examples:
+
+// Input:
+// LinkedList1: 1->2->3->4->5->6
+// LinkedList2: 99->59->42->20
+// Output: false
+// Explanation:
+
+// As shown in figure linkedlists are not identical.
+
+// Input:
+// LinkedList1: 1->2->3->4->5
+// LinkedList2: 1->2->3->4->5
+// Output: true
+// Explanation: 
+ 
+// As shown in figure both are identical.
+
+// Expected Time Complexity: O(n)
+// Expected Auxilliary Space: O(1)
+
+// Constraints:
+// 1 <= length of lists <= 103
+
+/*
+Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+// Function to check whether two linked lists are identical or not.
+bool areIdentical(struct Node *head1, struct Node *head2) {
+    // Code here
+    while(head1 && head2){
+        if(head1->data != head2->data)return false;
+        head1=head1->next;
+        head2=head2->next;
+    }
+    return head1 == head2 ? true : false;
+} 
