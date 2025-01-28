4304c3991ca267fe465c98454a9daed44e18b07c
Digvijay Chavan <chavandigvijay9081@gmail.com>
2025-01-28 21:34:12 +0530

    Medium

---
+++ Permutations of a String.cpp
@@ -0,0 +1,36 @@
+// Given a string s, which may contain duplicate characters, your task is to generate and return an array of all unique permutations of the string. You can return your answer in any order.
+
+// Examples:
+
+// Input: s = "ABC"
+// Output: ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
+// Explanation: Given string ABC has 6 unique permutations.
+// Input: s = "ABSG"
+// Output: ["ABGS", "ABSG", "AGBS", "AGSB", "ASBG", "ASGB", "BAGS", "BASG", "BGAS", "BGSA", "BSAG", "BSGA", "GABS", "GASB", "GBAS", "GBSA", "GSAB", "GSBA", "SABG", "SAGB", "SBAG", "SBGA", "SGAB", "SGBA"]
+// Explanation: Given string ABSG has 24 unique permutations.
+// Input: s = "AAA"
+// Output: ["AAA"]
+// Explanation: No other unique permutations can be formed as all the characters are same.
+// Constraints:
+// 1 <= s.size() <= 9
+// s contains only Uppercase english alphabets
+
+class Solution {
+  public:
+    vector<string> findPermutation(string &s) {
+        // Code here there
+        set<string> st;
+        vector<string> ans;
+        sort(s.begin(), s.end());
+        
+        do{
+            st.insert(s);
+        }while(next_permutation(s.begin(), s.end()));
+        
+        for(string p : st){
+            ans.push_back(p);
+        }
+        
+        return ans;
+    }
+};
