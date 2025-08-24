86e55b4e2a3405ef04587df4b0bf415030d3a0ff
Digvijay Chavan <chavandigvijay9081@gmail.com>
2025-08-25 00:57:38 +0530

    OPTIMISED

--- 0 - 1 Knapsack Problem.cpp
+++ 0 - 1 Knapsack Problem.cpp
@@ -1,16 +1,16 @@
-// Given n items, each with a specific weight and value, and a knapsack with a capacity of W, the task is to put the items in the knapsack such that the sum of weights of the items <= W and the sum of values associated with them is maximized. 
+// Given n items, each with a specific weight and value, and a knapsack with a capacity of W, the task is to put the items in the knapsack such that the sum of weights of the items <= W and the sum of values associated with them is maximized.
 
 // Note: You can either place an item entirely in the bag or leave it out entirely. Also, each item is available in single quantity.
 
 // Examples :
 
-// Input: W = 4, val[] = [1, 2, 3], wt[] = [4, 5, 1] 
+// Input: W = 4, val[] = [1, 2, 3], wt[] = [4, 5, 1]
 // Output: 3
 // Explanation: Choose the last item, which weighs 1 unit and has a value of 3.
-// Input: W = 3, val[] = [1, 2, 3], wt[] = [4, 5, 6] 
+// Input: W = 3, val[] = [1, 2, 3], wt[] = [4, 5, 6]
 // Output: 0
 // Explanation: Every item has a weight exceeding the knapsack's capacity (3).
-// Input: W = 5, val[] = [10, 40, 30, 50], wt[] = [5, 4, 2, 3] 
+// Input: W = 5, val[] = [10, 40, 30, 50], wt[] = [5, 4, 2, 3]
 // Output: 80
 // Explanation: Choose the third item (value 30, weight 2) and the last item (value 50, weight 3) for a total value of 80.
 // Constraints:
@@ -19,32 +19,118 @@
 // 1 ≤ val[i] ≤ 103
 // 1 ≤ wt[i] ≤ 103
 
-class Solution {
+class Solution
+{
     vector<vector<int>> dp;
-  public:
-    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+
+public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt)
+    {
         // code here
-        dp.assign(val.size()+1, vector<int>(W+1, -1));
+        dp.assign(val.size() + 1, vector<int>(W + 1, -1));
         int i = 0;
         int sumW = 0;
         return getMax(W, val, wt, i, sumW);
     }
-    
-    
-    
-    int getMax(int W, vector<int> &val, vector<int> &wt, int i, int sumW){
-        if(i >= val.size()){
+
+    int getMax(int W, vector<int> &val, vector<int> &wt, int i, int sumW)
+    {
+        if (i >= val.size())
+        {
             return 0;
         }
-        if(dp[i][sumW] != -1){
+        if (dp[i][sumW] != -1)
+        {
             return dp[i][sumW];
         }
         int take = 0;
-        if(wt[i]+sumW <= W){
-            take = val[i] + getMax(W, val, wt, i+1, wt[i]+sumW);
+        if (wt[i] + sumW <= W)
+        {
+            take = val[i] + getMax(W, val, wt, i + 1, wt[i] + sumW);
         }
-        int leave = getMax(W, val, wt, i+1, sumW);
-        
+        int leave = getMax(W, val, wt, i + 1, sumW);
+
         return dp[i][sumW] = max(leave, take);
     }
+};
+
+int knapsack(int W, vector<int> &val, vector<int> &wt)
+{
+    // code here
+    int n = val.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            int pick = 0;
+            if (wt[i - 1] <= j)
+            {
+                pick = val[i - 1] + dp[i - 1][j - wt[i - 1]];
+            }
+            dp[i][j] = max(pick, dp[i - 1][j]);
+        }
+    }
+
+    return dp[n][W];
+}
+
+class Solution
+{
+    // vector<vector<int>> dp;
+public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt)
+    {
+        // code here
+
+        int n = val.size();
+        vector<int> dp(W + 1, 0);
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = W; j >= wt[i - 1]; j--)
+            {
+                dp[j] = max(dp[j], dp[j - wt[i - 1]] + val[i - 1]);
+            }
+        }
+
+        return dp[W];
+
+        // int n = val.size();
+        // vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= W; j++){
+        //         int pick = 0;
+        //         if(wt[i-1] <= j){
+        //             pick = val[i-1] + dp[i-1][j-wt[i-1]];
+        //         }
+        //         dp[i][j] = max(pick, dp[i-1][j]);
+        //     }
+        // }
+
+        // return dp[n][W];
+
+        // dp.assign(val.size()+1, vector<int>(W+1, -1));
+        // int i = 0;
+        // int sumW = 0;
+        // return getMax(W, val, wt, i, sumW);
+    }
+
+    // int getMax(int W, vector<int> &val, vector<int> &wt, int i, int sumW){
+    //     if(i >= val.size()){
+    //         return 0;
+    //     }
+    //     if(dp[i][sumW] != -1){
+    //         return dp[i][sumW];
+    //     }
+    //     int take = 0;
+    //     if(wt[i]+sumW <= W){
+    //         take = val[i] + getMax(W, val, wt, i+1, wt[i]+sumW);
+    //     }
+    //     int leave = getMax(W, val, wt, i+1, sumW);
+
+    //     return dp[i][sumW] = max(leave, take);
+    // }
 };
