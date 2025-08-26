5a9f48df0d6549a27eb53bfe16d0d88b45c20e92
Digvijay Chavan <chavandigvijay9081@gmail.com>
2025-08-26 23:47:58 +0530

    Medium MOST OPTIMISED

--- Partition Equal Subset Sum.cpp
+++ Partition Equal Subset Sum.cpp
@@ -14,33 +14,39 @@
 // 1 ≤ arr.size ≤ 100
 // 1 ≤ arr[i] ≤ 200
 
-class Solution {
-  public:
-    bool equalPartition(vector<int>& arr) {
+class Solution
+{
+public:
+    bool equalPartition(vector<int> &arr)
+    {
         // code here
         int sum = 0;
-        for(int val : arr)sum += val;
-        if(sum%2 == 1)return false;
-    
+        for (int val : arr)
+            sum += val;
+        if (sum % 2 == 1)
+            return false;
+
         sum /= 2;
         int n = arr.size();
-        vector<bool> dp(sum+1, false);
-        
+        vector<bool> dp(sum + 1, false);
+
         dp[0] = true;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = sum; j >= arr[i]; j--){
-                dp[j] = dp[j] || dp[j-arr[i]];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = sum; j >= arr[i]; j--)
+            {
+                dp[j] = dp[j] || dp[j - arr[i]];
             }
         }
-        
+
         return dp[sum];
     }
-    
+
     //     vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
-        
+
     //     for(int i = 0; i < n; i++)dp[i][0] = true;
-        
+
     //     for(int i = 1; i <= n; i++){
     //         for(int j = 1; j <= sum; j++){
     //             bool notPick = dp[i-1][j];
@@ -51,7 +57,7 @@ class Solution {
     //             dp[i][j] = notPick || pick;
     //         }
     //     }
-        
+
     //     return dp[n][sum];
     // }
 };
