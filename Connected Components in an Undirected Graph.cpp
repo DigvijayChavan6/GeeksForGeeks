003c7287e9f93f0ec8d557a9925ce4ced8580655
Digvijay Chavan <chavandigvijay9081@gmail.com>
2025-12-07 20:00:20 +0530

    by DFS

---
+++ Connected Components in an Undirected Graph.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    void dfs(vector<int> &temp, int visited[], vector<vector<int>> graph, int V, int node){
+        visited[node] = 1;
+        temp.push_back(node);
+        for(int next : graph[node]){
+            if(visited[next] == 0){
+                dfs(temp, visited, graph, V, next);
+            }
+        }
+    }
+    
+  public:
+    vector<vector<int>> getComponents(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> graph(V);
+        vector<vector<int>> answer;
+        
+        for(auto edge : edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        
+        int visited[V] = {0};
+        
+        for(int i = 0; i < V; i++){
+            if(visited[i] == 0){
+                vector<int> temp;
+                dfs(temp, visited, graph, V, i);
+                answer.push_back(temp);
+            }
+        }
+        
+        return answer;
+    }
+};
