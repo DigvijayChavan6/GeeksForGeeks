06b5a0438deec347a9b997a906acb2b3533b3a0b
Digvijay Chavan <chavandigvijay9081@gmail.com>
2025-12-28 00:33:42 +0530

    Used Dijakstra's and DP to count

--- Number of Ways to Arrive at Destination.cpp
+++ Number of Ways to Arrive at Destination.cpp
@@ -46,3 +46,56 @@ class Solution {
         return dist[V-1][1];
     }
 };
+
+// New original code that uses Dijakstra's with DP to count paths of same minimum distance
+
+
+
+class Solution {
+  public:
+    int countPaths(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<pair<int, int>>> graph(V);
+        
+        for(int i = 0; i < edges.size(); i++){
+            graph[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+            graph[edges[i][1]].push_back({edges[i][0], edges[i][2]});
+        }
+        
+        vector<int> dist(V, INT_MAX);
+        vector<int> ways(V, 0);
+        
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+        
+        
+        dist[0] = 0;
+        ways[0] = 1;
+        
+        pq.push({0, 0});
+        
+        while(!pq.empty()){
+            int dt = pq.top().first;
+            int node = pq.top().second;
+            
+            pq.pop();
+            
+            if(dt > dist[node])continue;
+            
+            for(auto next : graph[node]){
+                if(dt + next.second < dist[next.first]){
+                    dist[next.first] = dt + next.second;
+                    ways[next.first] = ways[node];
+                    pq.push({dist[next.first], next.first});
+                }else if(dt + next.second == dist[next.first]){
+                    ways[next.first] += ways[node];
+                }
+            }
+        }
+        
+        return ways[V-1];
+    }
+};
