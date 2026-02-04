4daff9a6a34e9b661f40c3bfc1ce8d232105f49d
Digvijay Chavan <chavandigvijay9081@gmail.com>
2026-02-05 00:04:58 +0530

    Optimised

--- Chocolates Pickup.cpp
+++ Chocolates Pickup.cpp
@@ -116,6 +116,146 @@ class Solution {
 //     }
 
 
+class Solution {
+    // int getMax(vector<vector<int>> &grid, int r, int c1, int c2, int n, int m,
+    //             vector<vector<vector<int>>> &dp){
+    //     if(r >= n || c1 >= m || c2 >= m || r < 0 || c1 < 0 || c2 < 0){
+    //         return INT_MIN / 2;
+    //     }
+    //     if(r == n-1){
+    //         if(c1 == c2){
+    //             return grid[r][c1];
+    //         }else{
+    //             return grid[r][c1] + grid[r][c2];
+    //         }
+    //     }
+
+    //     if(dp[r][c1][c2] != -1)return dp[r][c1][c2];
+
+    //     int mx = INT_MIN/2;
+
+    //     int a[] = {-1, 0, 1};
+        
+    //     for(int i = 0; i < 3; i++){
+    //         for(int j = 0; j < 3; j++){
+    //             mx = max(mx, getMax(grid, r+1, c1+a[i], c2+a[j], n, m, dp));
+    //         }
+    //     }
+
+    //     mx += grid[r][c1];
+
+    //     if(c1 != c2){
+    //         mx += grid[r][c2];
+    //     }
+    //     return dp[r][c1][c2] = mx;
+    // }
+  public:
+    int maxChocolate(vector<vector<int>>& grid) {
+        // code here
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+        vector<vector<int>> prev(m, vector<int>(m, 0));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < m; j++){
+                if(i == j)dp[i][j] = grid[n-1][j];
+                else dp[i][j] = grid[n-1][i] + grid[n-1][j];
+            }
+        }
+        
+        for(int i = n-2; i >= 0; i--){
+            for(int j1 = 0; j1 < m; j1++){
+                for(int j2 = 0; j2 < m; j2++){
+                    int mx = INT_MIN / 2;
+                    for(int a = -1; a < 2; a++){
+                        for(int b = -1; b < 2; b++){
+                            
+                            int c1 = j1 + a;
+                            int c2 = j2 + b;
+                            
+                            if(c1 < 0 || c2 < 0 || c1 >= m || c2 >= m){
+                                continue;
+                            }
+                            
+                            int sum = dp[c1][c2];
+                            
+                            sum += grid[i][j1];
+                            
+                            if(j1 != j2){
+                                sum += grid[i][j2];
+                            }
+    
+                            mx = max(sum, mx);
+                        }
+                    }
+                    prev[j1][j2] = mx;
+                }
+            }
+            dp.swap(prev);
+        }
+        
+        return dp[0][m-1];
+        
+        //return getMax(grid, 0, 0, m-1, n, m, dp);
+    }
+};
+
+
+// int getMax(vector<vector<int>> &grid, int r, int c1, int c2, int n, int m,
+//                 vector<vector<vector<int>>> &dp){
+//         if(r >= n || c1 >= m || c2 >= m || r < 0 || c1 < 0 || c2 < 0){
+//             return INT_MIN / 2;
+//         }
+//         if(r == n-1){
+//             if(c1 == c2){
+//                 return grid[r][c1];
+//             }else{
+//                 return grid[r][c1] + grid[r][c2];
+//             }
+//         }
+
+//         if(dp[r][c1][c2] != -1)return dp[r][c1][c2];
+
+//         int mx = INT_MIN/2;
+
+//         int a[] = {-1, 0, 1};
+   
+//         for(int i = 0; i < 3; i++){
+//             for(int j = 0; j < 3; j++){
+//                 mx = max(mx, getMax(grid, r+1, c1+a[i], c2+a[j], n, m, dp));
+//             }
+//         }
+
+//         mx += grid[r][c1];
+
+//         if(c1 != c2){
+//             mx += grid[r][c2];
+//         }
+//         return dp[r][c1][c2] = mx;
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
