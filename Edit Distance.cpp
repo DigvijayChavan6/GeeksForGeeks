ebf6e919d7c46ee71f7f1b404c08819f58a8db91
Digvijay Chavan <chavandigvijay9081@gmail.com>
2026-04-01 00:00:22 +0530

    SPACE OPTI

--- Edit Distance.cpp
+++ Edit Distance.cpp
@@ -30,3 +30,40 @@ class Solution {
         return getMin(n-1, m-1, s1, s2, dp);
     }
 };
+
+
+class Solution {
+  public:
+    // Function to compute the edit distance between two strings
+    int editDistance(string& s1, string& s2) {
+        // code here
+        int n = s1.size();
+        int m = s2.size();
+        vector<int> curr(m+1, 0), prev(m+1, 0);
+        
+        for(int j = 0; j <= m; j++){
+            prev[j] = j;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            curr[0] = i;
+            for(int j = 1; j <= m; j++){
+                
+                int ins = INT_MAX, rem = ins, rep = ins, match = ins;
+        
+                if(s1[i-1] == s2[j-1]){
+                    match = prev[j-1];
+                }else{
+                    ins = 1 + curr[j-1];
+                    rem = 1 + prev[j];
+                    rep = 1 + prev[j-1];
+                }
+        
+                curr[j] = min(match, min(ins, min(rem, rep)));
+            }
+            prev = curr;
+        }
+        
+        return prev[m];
+    }
+};
